29e91fe6a0157cacd2a9dd075406e841772bf715
Heizal <patriciaheizal@gmail.com>
2024-09-21 20:19:57 +0200

    create GlobalExceptionHandler
    Signed-off-by: Heizal <patriciaheizal@gmail.com>

---
+++ src/main/java/com/example/taskmanager/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.example.taskmanager.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    //Handles custom ResourceNotFoundException
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    //Handles general exceptions
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        return new ResponseEntity<>("An unexpected error occured", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
