7a255a5c01ba38fe94926319fda61799c5b53736
Heizal <patriciaheizal@gmail.com>
2024-09-27 15:24:06 +0200

    rearrange structure for register json
    Signed-off-by: Heizal <patriciaheizal@gmail.com>

--- src/main/java/com/example/taskmanager/dto/UserRegistrationDto.java
+++ src/main/java/com/example/taskmanager/dto/UserRegistrationDto.java
@@ -14,20 +14,21 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserRegistrationDto {
-    @NotBlank(message = "Username is required")
-    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
-    private String username;
 
     @NotBlank(message = "Email is required")
     @Email(message = "Please provide a valid email address")
     private String email;
 
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
+    private String username;
+
     @NotBlank(message = "Password is required")
     @Size(min = 6, message = "Password must be at least 6 characters long")
     private String password;
 
-    @NotBlank(message = "Password confirmation is required")
+    /*@NotBlank(message = "Password confirmation is required")
     @Size(min = 6, message = "Password confirmation must be at least 6 characters long")
-    private String confirmPassword;
+    private String confirmPassword;*/
 
 }
